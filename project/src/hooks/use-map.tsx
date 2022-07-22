eeed950a0fc325f796aa784575510d870cfba1a7
alexander1970 <44961623+alexander1970@users.noreply.github.com>
2022-07-22 15:32:48 +0300

    5.15. Больше подробностей (часть 2)

--- project/src/hooks/use-map.tsx
+++ project/src/hooks/use-map.tsx
@@ -2,7 +2,7 @@ import { Map, TileLayer } from 'leaflet';
 import { MutableRefObject, useEffect, useState } from 'react';
 import { PointInMap } from '../types/offer';
 
-const ZOOM = 10;
+const ZOOM = 12;
 const LAYER_TITLE = 'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png';
 const LAYER_ATTR = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>';
 
